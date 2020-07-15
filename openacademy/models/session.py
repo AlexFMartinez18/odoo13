96392ce1876a81deb20bce5f511e9c3265058dd1
Alex Martinez <ferchomar60@gmail.com>
2020-07-15 16:02:05 -0600

    [ADD] Many2one relations

--- openacademy/models/session.py
+++ openacademy/models/session.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
+    responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsible", index=True)
+
 class Session(models.Model):
     _name = 'openacademy.session'
     _description = "OpenAcademy Sessions"
@@ -9,3 +12,7 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course',
+        ondelete='cascade', string="Course", required=True)
